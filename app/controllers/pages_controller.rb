db96824732bcb45bb5cc319b428deba6e7c0d14d
Denzel Gent <deeman4yah@msn.com>
2011-10-13 11:45:27 -0400

    Added a site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
+    @message = "You are at the Title Page"
   end
 
   def contact
+    @title = "Contact"
+  end
+  
+  def about
+    @title = "About"
   end
 
 end
