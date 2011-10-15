4e4c0c553cbde6adaa1acbcab1af1067b4c92eee
Denzel Gent <deeman4yah@msn.com>
2011-10-15 18:27:34 -0400

    Added a stub show page

--- db/migrate/20111013232525_add_email_uniqueness_index.rb
+++ db/migrate/20111013232525_add_email_uniqueness_index.rb
@@ -1,10 +1,11 @@
 class AddEmailUniquenessIndex < ActiveRecord::Migration
     
-    def self.up 
-      add_index :users, :email, :unique => true 
+    def self.up
+      add_index :users, :email, :unique => true
     end
-  
-    def self.down 
+
+    def self.down
       remove_index :users, :email
-    end 
+    end
   end
+
