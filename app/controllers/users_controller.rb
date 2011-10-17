425eb5d53a96761433982a4af3e8b22af6ac2971
Denzel Gent <deeman4yah@msn.com>
2011-10-17 19:39:10 -0400

    improved the user page and added a gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
   
+  def show 
+     @user = User.find(params[:id])
+     @title = @user.name + "<script>"
+   end
+  
   def new 
     @title = "Sign up"
     
   end 
   
-  def show 
-    @user = User.find(params[:id])
-  end
+ 
+  
+  
   
 end
