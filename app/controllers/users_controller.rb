3ee29e4f4ff387c654491d4647a26afaaef00cfb
Denzel Gent <deeman4yah@msn.com>
2011-10-18 09:04:56 -0400

    Filterfor Editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   
+  before_filter :authenticate, :only => [:edit, :update] 
+  before_filter :correct_user, :only => [:edit, :update]
+  
   def show 
      @user = User.find(params[:id])
      @title = @user.name + "<script>"
@@ -37,6 +40,19 @@ class UsersController < ApplicationController
     render 'edit'
   end
 end
+
+private
+
+def authenticate
+deny_access unless signed_in?
+end
+
+def correct_user
+@user = User.find(params[:id]) 
+redirect_to(root_path) unless current_user?(@user)
+end
+
+
 end
   
 
